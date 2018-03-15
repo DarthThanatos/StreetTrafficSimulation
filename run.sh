6fe3d61f0604cc5aaafcb6fb9f5318c45996a99e
DarthThanatos <robert.bielas@interia.pl>
2018-03-15 11:39:54 +0100

    bash script

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ find -name "*.java" > sources
 javac -d bin -cp "lib/*:bin" @sources
 java -cp "bin:lib/*" display.TrafficDisplay
 # java -cp "out/production/StreetTrafficSimulation;lib/*" display.TrafficDisplay
-# ^ if sb wished to run sth produced by intellij ide
+# ^ if sb wished to run sth produced by intellij IDE
