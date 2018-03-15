e3bc1de8d788ffbfc9f65fedf11d581efaa29239
DarthThanatos <robert.bielas@interia.pl>
2018-03-15 11:08:42 +0100

    better script

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-java -cp "out/production/StreetTrafficSimulation;lib/*" display.TrafficDisplay 
+md bin
+dir /s /B *.java > sources
+javac -d bin -cp "lib/*;bin" @sources
+java -cp "bin;lib/*" display.TrafficDisplay 
+rem java -cp "out/production/StreetTrafficSimulation;lib/*" display.TrafficDisplay  
