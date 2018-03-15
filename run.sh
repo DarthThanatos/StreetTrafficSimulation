fe478031ab3ed6f18191a82f74e31ddd0b1dcca5
DarthThanatos <robert.bielas@interia.pl>
2018-03-15 11:20:55 +0100

    bash script

---
+++ run.sh
@@ -0,0 +1,5 @@
+mkdir bin
+find -name "*.java" > sources
+javac -d bin -cp "lib/*;bin" @sources
+java -cp "bin;lib/*" display.TrafficDisplay
+# java -cp "out/production/StreetTrafficSimulation;lib/*" display.TrafficDisplay
